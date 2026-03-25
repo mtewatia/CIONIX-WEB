20861bb12b2e786c6bfa01102b3df88219714073
mtewatia <vb.netheaven@gmail.com>
2026-03-25 14:08:04 +0530

    Add CORS headers to allow frontend API calls

--- server/contact-api.js
+++ server/contact-api.js
@@ -8,6 +8,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(bodyParser.json());
 
 const transporter = nodemailer.createTransport({
